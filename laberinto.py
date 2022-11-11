aa8a8d3e3851e982e4a88b4bfaba6f3ec448b5e3
Eduardo Serna <eserna@baufest.com>
2022-11-11 00:41:14 -0600

    Update Task

--- laberinto.py
+++ laberinto.py
@@ -1,16 +1,61 @@
-import copy
 from time import sleep
-import os
 
 
 def dibujarMatriz(matriz):
-    os.system('cls')
+    """
+    Show the matrix in the screen
+    :param matriz: any 2D matrix of any size
+    """
+    print("\n"*80)
     print("Best Solution:")
     for renglon in matriz:
         for columna in renglon:
             print(columna, end='')
         print()
     sleep(0.5)
+
+
+def copyMatrix(matriz):
+    """
+    Copy a matrix
+    :param matriz: any 2D matrix of any size
+    :return: a copy of the matrix
+    """
+    matrizCopiada = []
+    for renglon in matriz:
+        matrizCopiada.append([])
+        for columna in renglon:
+            matrizCopiada[-1].append(columna)
+    return matrizCopiada
+
+
+def copyList(lista):
+    """
+    Copy a list
+    :param lista: any list of any size
+    :return: a copy of the list
+    """
+    listaCopiada = []
+    for elemento in lista:
+        listaCopiada.append(elemento)
+    return listaCopiada
+
+
+def checkIfNextToSolution(matriz, xcoordinate, ycoordinate):
+    """
+    Check if the current position is next to the solution
+    :param matriz: any 2D matrix of any size
+    :param xcoordinate: the x coordinate of the current position
+    :param ycoordinate: the y coordinate of the current position
+    :return: True if the current position is next to the solution, False otherwise
+    """
+    if matriz[xcoordinate][ycoordinate - 1] == 'S' or \
+            matriz[xcoordinate - 1][ycoordinate] == 'S' or \
+            tempMatrix[xcoordinate][ycoordinate + 1] == 'S' or \
+            tempMatrix[xcoordinate + 1][ycoordinate] == 'S':
+        return True
+    else:
+        return False
 
 
 labyrinth = [['x', 'x', 'x', 'x', 'x', 'x'],
@@ -31,138 +76,11 @@ labyrinth = [['x', 'x', 'x', 'x', 'x', 'x'],
 #              ['x', 'E', ' ', ' ', ' ', ' ', 'S', 'x'],
 #              ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
 
-tempMatrix = copy.deepcopy(labyrinth)
-
-entrance = [0, 0]
-salida = [0, 0]
-checkpoint = [[]]
-saveBoard = []
-saveNumberOfSteps = []
-numberOfSteps = 0
+tempMatrix = copyMatrix(labyrinth)
 optimalSteps = 10000
-paths = [[]]
-numberOfCheckpoints = 10000
+solution = [[]]
 
-row = 0
-while row < len(labyrinth):
-    column = 0
-    while column < len(labyrinth[row]):
-        if labyrinth[row][column] == 'E':
-            entrance = [row, column]
-        elif labyrinth[row][column] == 'S':
-            salida = [row, column]
-        column += 1
-    row += 1
-
-currentLocation = copy.deepcopy(entrance)
-
-pathLeft = True
-pathRight = True
-pathUp = True
-pathDown = True
-while numberOfCheckpoints > 0 or pathLeft is True or pathRight is True or pathUp is True or pathDown is True:
-    numberOfPaths = 0
-    if tempMatrix[currentLocation[0]][currentLocation[1] - 1] == ' ':
-        pathLeft = True
-        numberOfPaths += 1
-    else:
-        pathLeft = False
-    if tempMatrix[currentLocation[0] - 1][currentLocation[1]] == ' ':
-        pathUp = True
-        numberOfPaths += 1
-    else:
-        pathUp = False
-    if tempMatrix[currentLocation[0]][currentLocation[1] + 1] == ' ':
-        pathRight = True
-        numberOfPaths += 1
-    else:
-        pathRight = False
-    if tempMatrix[currentLocation[0] + 1][currentLocation[1]] == ' ':
-        pathDown = True
-        numberOfPaths += 1
-    else:
-        pathDown = False
-
-    if numberOfPaths > 1 and currentLocation != checkpoint[-1]:
-        tempList = copy.deepcopy(currentLocation)
-        checkpoint.append(tempList)
-        paths.append([pathLeft, pathUp, pathRight, pathDown])
-        numberOfCheckpoints = len(checkpoint)
-        tempList = copy.deepcopy(tempMatrix)
-        saveBoard.append(tempList)
-        saveNumberOfSteps.append(numberOfSteps)
-
-    if not checkpoint[0]:
-        checkpoint.pop(0)
-        paths.pop(0)
-        numberOfCheckpoints = len(checkpoint)
-
-    if currentLocation == checkpoint[-1]:
-        pathLeft = paths[-1][0]
-        pathUp = paths[-1][1]
-        pathRight = paths[-1][2]
-        pathDown = paths[-1][3]
-
-    if pathLeft is True:
-        if currentLocation == checkpoint[-1]:
-            paths[-1][0] = False
-        currentLocation[1] -= 1
-        numberOfSteps += 1
-        tempMatrix[currentLocation[0]][currentLocation[1]] = '.'
-        dibujarMatriz(tempMatrix)
-    elif pathUp is True:
-        if currentLocation == checkpoint[-1]:
-            paths[-1][1] = False
-        currentLocation[0] -= 1
-        numberOfSteps += 1
-        tempMatrix[currentLocation[0]][currentLocation[1]] = '.'
-        dibujarMatriz(tempMatrix)
-    elif pathRight is True:
-        if currentLocation == checkpoint[-1]:
-            paths[-1][2] = False
-        currentLocation[1] += 1
-        numberOfSteps += 1
-        tempMatrix[currentLocation[0]][currentLocation[1]] = '.'
-        dibujarMatriz(tempMatrix)
-    elif pathDown is True:
-        if currentLocation == checkpoint[-1]:
-            paths[-1][3] = False
-        currentLocation[0] += 1
-        numberOfSteps += 1
-        tempMatrix[currentLocation[0]][currentLocation[1]] = '.'
-        dibujarMatriz(tempMatrix)
-    elif tempMatrix[currentLocation[0]][currentLocation[1] - 1] == 'S' or tempMatrix[currentLocation[0] - 1][currentLocation[1]] == 'S' or tempMatrix[currentLocation[0]][currentLocation[1] + 1] == 'S' or tempMatrix[currentLocation[0] + 1][currentLocation[1]] == 'S':
-        if numberOfSteps < optimalSteps:
-            optimalSteps = numberOfSteps
-            solution = copy.deepcopy(tempMatrix)
-        if len(checkpoint) > 0:
-            currentLocation = copy.deepcopy(checkpoint[-1])
-            tempMatrix = copy.deepcopy(saveBoard[-1])
-            numberOfCheckpoints = len(checkpoint)
-            numberOfSteps = saveNumberOfSteps[-1]
-        else:
-            numberOfCheckpoints = 0
-    else:
-        if len(checkpoint) > 0:
-            currentLocation = copy.deepcopy(checkpoint[-1])
-            tempMatrix = copy.deepcopy(saveBoard[-1])
-            numberOfCheckpoints = len(checkpoint)
-            numberOfSteps = saveNumberOfSteps[-1]
-        else:
-            numberOfCheckpoints = 0
-
-    if currentLocation == checkpoint[-1]:
-        if paths[-1] == [False, False, False, False]:
-            checkpoint = copy.deepcopy(checkpoint[:-1])
-            paths = copy.deepcopy(paths[:-1])
-            saveBoard = copy.deepcopy(saveBoard[:-1])
-            saveNumberOfSteps = copy.deepcopy(saveNumberOfSteps[:-1])
-            numberOfCheckpoints = len(checkpoint)
-            if numberOfCheckpoints > 0:
-                currentLocation = copy.deepcopy(checkpoint[-1])
-                tempMatrix = copy.deepcopy(saveBoard[-1])
-                numberOfSteps = saveNumberOfSteps[-1]
-
+# TODO: Write your code here...
 
 dibujarMatriz(solution)
 print("Optimum number of steps:", optimalSteps)
